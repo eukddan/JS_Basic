23977b9cdbaa5b13ca008dfa9bc4a76efc25ce3f
김유찬 <dkejdlcl123@naver.com>
2023-09-23 21:57:15 +0900

    dataset

--- tab.js
+++ tab.js
@@ -2,11 +2,20 @@ var btn = $(".tab-button");
 var content = $(".tab-content");
 var classNum = $(".tab-button").length;
 
-for (let i = 0; i < classNum; i++) {
-  btn.eq(i).on("click", function () {
-    btn.removeClass("orange");
-    btn.eq(i).addClass("orange");
-    content.removeClass("show");
-    content.eq(i).addClass("show");
-  });
+// for (let i = 0; i < classNum; i++) {
+//   btn.eq(i).on("click", function () {
+//     tabOpen(i);
+//   });
+// }
+
+$(".list").click(function (e) {
+  console.log(e.target.dataset.id);
+  tabOpen(e.target.dataset.id);
+});
+
+function tabOpen(i) {
+  btn.removeClass("orange");
+  btn.eq(i).addClass("orange");
+  content.removeClass("show");
+  content.eq(i).addClass("show");
 }
